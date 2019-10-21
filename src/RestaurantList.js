6452b0af0aa6f87fd96aebaf94d68321b20e6fee
David <dhahn123@gmail.com>
2019-10-20 23:21:44 -0700

    add no restaurant case

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { Collection, CollectionItem } from 'react-materialize';
 
 const RestaurantList = ({ restaurantNames }) => (
-    <Collection>
+    <Collection header = "Restaurants">
         {
-            restaurantNames.map(restaurantName => (
-                <CollectionItem key={restaurantName}>
-                    {restaurantName}
-                </CollectionItem>
-            ))
+            restaurantNames.length === 0
+                ? (
+                <CollectionItem
+                    style={{ color: 'gray' }}
+                >None Added</CollectionItem>
+                )
+                : (
+                    restaurantNames.map(restaurantName => (
+                        <CollectionItem key={restaurantName}>
+                            {restaurantName}
+                        </CollectionItem>
+                    ))
+            )
         }
     </Collection>
 );
